26986118acb8070c77173d9544c084c56435a212
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-12 21:16:40 -0500

    refactors CategoryMenu

--- client/src/components/CategoryMenu/index.jsx
+++ client/src/components/CategoryMenu/index.jsx
@@ -1,6 +1,6 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
-import { useStoreContext } from '../../utils/GlobalState';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
@@ -9,7 +9,8 @@ import { QUERY_CATEGORIES } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
 
   const { categories } = state;
 
@@ -54,13 +55,13 @@ function CategoryMenu() {
           {item.name}
         </button>
       ))}
-      <button
+      {/* <button
         onClick={() => {
           handleClick('');
         }}
       >
         All
-      </button>
+      </button> */}
     </div>
   );
 }
